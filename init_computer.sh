07c375262c64d23730e83185533fb1ac1bb408b6
Junhan <junhan@g2.nctu.edu.tw>
2020-04-06 23:20:12 +0800

    add file, for new computer install

---
+++ init_computer.sh
@@ -0,0 +1,27 @@
+echo "[apt] install start"
+./tutorial/apt/install_apt.sh
+
+echo "[p3 venv]"
+python3 -m venv ~/myvenv
+
+echo "[config] Copy bash_profile"
+cp -r ./config/vim ~/.vim
+mv ~/.vim/vimrc ~/.vimrc
+
+echo "[config] Copy tmux.conf"
+cp ./configs/tmux.conf ~/.tmux.conf
+
+echo "[config] Copy bash_profile"
+cp ./configs/bash_profile ~/.bash_profile
+
+echo "[config] Copy gitconfig"
+cp ./config/gitconfig ~/.gitconfig
+
+echo "[config] Copy SSH config"
+if [ ! -d "~/.ssh" ]; then
+    mkdir ~/.ssh
+fi
+cp ./config/ssh_config ~/.ssh/config
+ssh-keygen -t rsa
+
+
